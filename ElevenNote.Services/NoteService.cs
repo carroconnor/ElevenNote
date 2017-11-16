a0d0ff406f54a85c1e938d9bcad6386ae518bb17
Carr OConnor <carr.oconnor@gmail.com>
2017-11-16 12:09:56 -0500

    details, edit, refactor

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -68,7 +68,7 @@ namespace ElevenNote.Services
 
             using (var ctx = new ElevenNoteDBContext())
             {
-                entity = ctx.Notes.SingleOrDefault(e => e.NoteId == id && e.OwnerId == _userId);
+                entity = GetNoteById(ctx, id);
             }
 
             if (entity == null) return new NoteDetailModel();
@@ -81,6 +81,42 @@ namespace ElevenNote.Services
                 CreatedUtc = entity.CreatedUtc,
                 ModifiedUtc = entity.ModifiedUtc
             };
+        }
+
+        public bool UpdateNote(NoteEditModel model)
+        {
+
+            using (var ctx = new ElevenNoteDBContext())
+            {
+                var entity = GetNoteById(ctx, model.NoteId);
+
+                if (entity == null) return false;
+
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTime.UtcNow;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        private NoteEntity GetNoteById(ElevenNoteDBContext context, int noteId)
+        {
+            return context.Notes.SingleOrDefault(e => e.NoteId == noteId && e.OwnerId == _userId);
+        }
+
+        public bool DeleteNote(int noteId)
+        {
+            using(var ctx = new ElevenNoteDBContext())
+            {
+                var entity = GetNoteById(ctx, noteId);
+
+                if (entity == null) return false;
+
+                ctx.Notes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
         }
     }
 }
